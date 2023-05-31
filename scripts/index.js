55e85cf48e8b34d7022bb463e07927aea4a66de1
QosimjonEliev <128938129+QosimjonEliev@users.noreply.github.com>
2023-05-31 09:13:56 -0400

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -29,12 +29,32 @@ const initialCards = [
 
   const profileEditButton = document.querySelector("#profile-edit-button");
   const profileEditModal = document.querySelector("#profile-edit-modal");
-  const modalCloseButton = document.querySelector("#modal-close-button");
+  const profileEditCloseButton = profileEditModal.querySelector(".modal-close-button");
+  const profileTitle = document.querySelector(".profile__title");
+  const profileDescription = document.querySelector(".profile__description");
+  const profileTitleInput = document.querySelector("#profile-title-input");
+  const profileDescriptionInput = document.querySelector(
+    "#profile-description-input");
+  const profileEditForm = profileEditModal.querySelector(".madal__form");
+
+  function closePopop(){
+   profileEditModal.classList.remove("modal_opened");
+  }
   
   profileEditButton.addEventListener("click", () => {
+   profileTitleInput.value = profileTitle.textContent;
+   profileDescriptionInput.value = profileDescription.textContent;
     profileEditModal.classList.add("modal_opened");
   });
   
-  modalCloseButton.addEventListener("click", () => {
-    profileEditModal.classList.remove("modal_opened");
+  profileEditCloseButton.addEventListener("click", () => {
+    closePopop();
+  });
+   
+
+  profileEditForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    profileTitle.textContent = profileTitleInput.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopop();
   });
