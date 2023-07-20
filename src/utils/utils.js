1a2c8caea79a92171e6775ee5274ff6239776dd3
QosimjonEliev <128938129+QosimjonEliev@users.noreply.github.com>
2023-07-20 12:12:29 -0400

    Add files via upload

---
+++ src/utils/utils.js
@@ -0,0 +1,28 @@
+/* Close by click & Escape button*/ 
+function closePopup(modal) {
+    modal.classList.remove("modal_opened");
+    document.removeEventListener("keydown", handleEscape);
+    modal.removeEventListener("mousedown", handleModalClose);
+  }
+  function openPopup(modal) {
+    modal.classList.add("modal_opened");
+    document.addEventListener("keydown", handleEscape);
+    modal.addEventListener("mousedown", handleModalClose);
+  }
+  
+  function handleEscape(evt) {
+    if (evt.key === "Escape") {
+      const openedPopup = document.querySelector(".modal_opened");
+      closePopup(openedPopup);
+    }
+  }
+  
+  function handleModalClose(evt) {
+    if (
+      evt.target.classList.contains("modal__close") ||
+      evt.target.classList.contains("modal_opened")
+    ) {
+      closePopup(evt.currentTarget);
+    }
+  }
+  export{openPopup, closePopup, handleEscape, handleModalClose};
