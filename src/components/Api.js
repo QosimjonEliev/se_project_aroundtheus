14a393b270390e29709789840b89709dee74577e
QosimjonEliev <128938129+QosimjonEliev@users.noreply.github.com>
2023-08-26 12:34:35 -0400

    Add files via upload

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,97 +1,96 @@
 export default class Api { 
-    constructor({ baseUrl, headers }) {
-        this._baseUrl = baseUrl;
-        this._headers = headers;
-    }
+  constructor({ baseUrl, headers }) {
+      this._baseUrl = baseUrl;
+      this._headers = headers;
+  }
 
-    _checResponse(res) {
-        if (res.ok) {
-            return res.json();
-        }
-        return Promise.reject(`Error: ${res.status}`);
-    }
-
-    _request(name, link) {
-        return fetch(name, link).then(this._checResponse);
-    }
-
-    getUserInfo(){
-        return this._request(`${this._baseUrl}/users/me`, {
-          headers: this._headers,
-        }).finally(() => {
-          console.log("Done with user info");
-        });
+  _checResponse(res) {
+      if (res.ok) {
+          return res.json();
       }
-      getInitialCards() {
-        return this._request(`${this._baseUrl}/cards`, {headers: this._headers })
-        .then((result) => {
-            return result;
+      return Promise.reject(`Error: ${res.status}`);
+  }
+
+  _request(name, link) {
+      return fetch(name, link).then(this._checResponse);
+  }
+
+  getUserInfo(){
+      return this._request(`${this._baseUrl}/users/me`, {
+        headers: this._headers,
+      }).finally(() => {
+        console.log("Done with user info");
+      });
+    }
+    getInitialCards() {
+      return this._request(`${this._baseUrl}/cards`, {headers: this._headers })
+      .then((result) => {
+          return result;
+      })
+      .finally(() => {
+          console.log("Don with initial cards");
+      });
+    }
+
+   updateProfileInfo(name, about) {
+      return fetch(`${this._baseUrl}/users/me`, {
+        method: "PATCH",
+        headers: this._headers,
+        body: JSON.stringify({
+          name: name,
+          about: about,
+        }),
+      }).then(this._checkResponse);
+    }
+
+    avatarInformation(inputValues) {
+      return fetch(`${this._baseUrl}/users/me/avatar/`, {
+        method: "PATCH",
+        headers: this._headers,
+        body: JSON.stringify({ avatar: inputValues }),
+      }).then(this._checkResponse);
+    }
+  
+    addNewCardInformation(name, link) {
+      return this._request(`${this._baseUrl}/cards`, {
+        method: "POST",
+        headers: this._headers,
+        body: JSON.stringify({
+         name: name,
+         link: link,
+        }),
+      })
+        .then((response) => {
+          return response;
         })
         .finally(() => {
-            console.log("Don with initial cards");
+          console.log("Done adding New Card Information from Server");
         });
-      }
-
-     updateProfileInfo(name, about) {
-        return fetch(`${this._baseUrl}/users/me`, {
-          method: "PATCH",
-          headers: this._headers,
-          body: JSON.stringify({
-            name: name,
-            about: about,
-          }),
-        }).then(this._checkResponse);
-      }
-
-      avatarInformation(inputValues) {
-        return fetch(`${this._baseUrl}/users/me/avatar/`, {
-          method: "PATCH",
-          headers: this._headers,
-          body: JSON.stringify({ avatar: inputValues }),
-        }).then(this._checkResponse);
-      }
-    
-      addNewCardInformation({ name, link }) {
-        return this._request(`${this._baseUrl}/cards`, {
-          method: "POST",
-          headers: this._headers,
-          body: JSON.stringify({
-           name: name,
-           link: link,
-          }),
-        })
-          .then((response) => {
-            return response;
-          })
-          .finally(() => {
-            console.log("Done adding New Card Information from Server");
-          });
-      }
-
-      deleteCardInformation(cardId) {
-        return this._request(`${this._baseUrl}/cards/${cardId}`, {
-          method: "DELETE",
-          headers: this._headers,
-        })
-          .then((res) => {
-            return this._checkResponse(res);
-          })
-          .finally(() => {
-            console.log("Done deleting card");
-          });
-      }
-
-      likesAddInformation(cardId) {
-        return this._request(`${this._baseUrl}/cards/likes/${cardId}`, {
-          method: "PUT",
-          headers: this._headers,
-        });
-      }
-      likesRemoveInformation(cardId) {
-        return this._request(`${this._baseUrl}/cards/likes/${cardId}`, {
-          method: "DELETE",
-          headers: this._headers,
-        });
-      }
     }
 
+    deleteCardInformation(cardId) {
+      return this._request(`${this._baseUrl}/cards/${cardId}`, {
+        method: "DELETE",
+        headers: this._headers,
+      })
+        .then((res) => {
+          return this._checkResponse(res);
+        })
+        .finally(() => {
+          console.log("Done deleting card");
+        });
+    }
+
+    likesAddInformation(cardId) {
+      return this._request(`${this._baseUrl}/cards/likes/${cardId}`, {
+        method: "PUT",
+        headers: this._headers,
+      });
+    }
+    likesRemoveInformation(cardId) {
+      return this._request(`${this._baseUrl}/cards/likes/${cardId}`, {
+        method: "DELETE",
+        headers: this._headers,
+      });
+    }
+  }
